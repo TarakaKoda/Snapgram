4c6d0d19526b7c05d91c3f973a7754e0d5c8c7c7
TarakaKoda <taraka.koda@gmail.com>
2024-01-09 22:20:45 +0530

    Build update profile page.

--- src/_root/pages/UpdateProfile.tsx
+++ src/_root/pages/UpdateProfile.tsx
@@ -1,8 +1,22 @@
+import ProfileForm from "@/components/forms/ProfileForm";
 
 const UpdateProfile = () => {
   return (
-    <div>UpdateProfile</div>
-  )
-}
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="flex-start w-full max-w-5xl justify-start gap-3 ">
+          <img
+            src="/assets/icons/add-post.svg"
+            alt="add post"
+            width={36}
+            height={36}
+          />
+          <h2 className="h3-bold md:h2-bold w-full text-left">Edit Post</h2>
+        </div>
+        <ProfileForm />
+      </div>
+    </div>
+  );
+};
 
-export default UpdateProfile
+export default UpdateProfile;
