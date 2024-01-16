c202172d5f4ede5c247d5b1e3873b768a60a56ad
TarakaKoda <taraka.koda@gmail.com>
2024-01-16 21:10:28 +0530

    Build related post component in post details component.

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,25 +1,65 @@
+import { useParams, Link, useNavigate } from "react-router-dom";
+
+import {
+  useGetPostById,
+  useGetUserPosts,
+  useDeletePost,
+} from "@/lib/react-query/queriesAndMutations";
+import { multiFormatDateString } from "@/lib/utils";
+import { useUserContext } from "@/context/AuthContext";
+import { Button } from "@/components/ui/button";
 import { Loader } from "@/components/shared";
 import PostStats from "@/components/shared/PostStats";
-import { Button } from "@/components/ui/button";
-import { useUserContext } from "@/context/AuthContext";
-import { useGetPostById } from "@/lib/react-query/queriesAndMutations";
-import { formatDateString } from "@/lib/utils";
-import { Link, useParams } from "react-router-dom";
+import GridPostList from "@/components/shared/GridPostList";
 
 const PostDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
-  const { data: post, isPending } = useGetPostById(id || "");
   const { user } = useUserContext();
 
-  const handleDeletePost = () => {};
+  const { data: post, isLoading } = useGetPostById(id || "");
+  const { data: userPosts, isLoading: isUserPostLoading } = useGetUserPosts(
+    post?.creator.$id,
+  );
+  const { mutate: deletePost } = useDeletePost();
+
+  const relatedPosts = userPosts?.documents.filter(
+    (userPost) => userPost.$id !== id,
+  );
+
+  const handleDeletePost = () => {
+    deletePost({ postId: id || "", imageId: post?.imageId });
+    navigate(-1);
+  };
 
   return (
     <div className="post_details-container">
-      {isPending ? (
+      <div className="hidden w-full max-w-5xl md:flex">
+        <Button
+          onClick={() => navigate(-1)}
+          variant="ghost"
+          className="shad-button_ghost"
+        >
+          <img
+            src={"/assets/icons/back.svg"}
+            alt="back"
+            width={24}
+            height={24}
+          />
+          <p className="small-medium lg:base-medium">Back</p>
+        </Button>
+      </div>
+
+      {isLoading || !post ? (
         <Loader />
       ) : (
         <div className="post_details-card">
-          <img src={post?.imageUrl} alt="post" className="post_details-img" />
+          <img
+            src={post?.imageUrl}
+            alt="creator"
+            className="post_details-img"
+          />
+
           <div className="post_details-info">
             <div className="flex-between w-full">
               <Link
@@ -28,49 +68,50 @@ const PostDetails = () => {
               >
                 <img
                   src={
-                    post?.creator?.imageUrl ||
+                    post?.creator.imageUrl ||
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
                   className="h-8 w-8 rounded-full lg:h-12 lg:w-12"
                 />
-
-                <div className="flex flex-col">
+                <div className="flex flex-col gap-1">
                   <p className="base-medium lg:body-bold text-light-1">
                     {post?.creator.name}
                   </p>
-                  <div className="flex items-center gap-2 text-light-3">
-                    <p className="subtle-semibold lg:small-regular">
-                      {formatDateString(post?.$createdAt || "")}
+                  <div className="flex-center gap-2 text-light-3">
+                    <p className="subtle-semibold lg:small-regular ">
+                      {multiFormatDateString(post?.$createdAt)}
                     </p>
-                    -
+                    •
                     <p className="subtle-semibold lg:small-regular">
                       {post?.location}
                     </p>
                   </div>
                 </div>
               </Link>
-              <div className="flex-center gap-1">
+
+              <div className="flex-center gap-4">
                 <Link
                   to={`/update-post/${post?.$id}`}
                   className={`${user.id !== post?.creator.$id && "hidden"}`}
                 >
                   <img
-                    src="/assets/icons/edit.svg"
+                    src={"/assets/icons/edit.svg"}
                     alt="edit"
                     width={24}
                     height={24}
                   />
                 </Link>
+
                 <Button
                   onClick={handleDeletePost}
                   variant="ghost"
-                  className={`ghost_details-delete_btn ${
+                  className={`ost_details-delete_btn ${
                     user.id !== post?.creator.$id && "hidden"
                   }`}
                 >
                   <img
-                    src="/assets/icons/delete.svg"
+                    src={"/assets/icons/delete.svg"}
                     alt="delete"
                     width={24}
                     height={24}
@@ -78,23 +119,42 @@ const PostDetails = () => {
                 </Button>
               </div>
             </div>
+
             <hr className="w-full border border-dark-4/80" />
+
             <div className="small-medium lg:base-regular flex w-full flex-1 flex-col">
               <p>{post?.caption}</p>
               <ul className="mt-2 flex gap-1">
-                {post?.tags.map((tag: string) => (
-                  <li key={tag} className="text-light-3">
+                {post?.tags.map((tag: string, index: string) => (
+                  <li
+                    key={`${tag}${index}`}
+                    className="small-regular text-light-3"
+                  >
                     #{tag}
                   </li>
                 ))}
               </ul>
             </div>
+
             <div className="w-full">
               <PostStats post={post} userId={user.id} />
             </div>
           </div>
         </div>
       )}
+
+      <div className="w-full max-w-5xl">
+        <hr className="w-full border border-dark-4/80" />
+
+        <h3 className="body-bold md:h3-bold my-10 w-full">
+          More Related Posts
+        </h3>
+        {isUserPostLoading || !relatedPosts ? (
+          <Loader />
+        ) : (
+          <GridPostList posts={relatedPosts} />
+        )}
+      </div>
     </div>
   );
 };
